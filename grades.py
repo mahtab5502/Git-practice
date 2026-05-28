b3e3565690948adb8599c0066a6ca046b225c54d
mahtab5502 <mahtabdehkordi20408m@gmail.com>
2026-05-28 18:07:25 +0330

    Add result print function

--- grades.py
+++ grades.py
@@ -1,10 +1,11 @@
-SUBJECTS = ["Math", "English", "Science"]
+SUBJECTS = ["국어", "영어", "수학", "과탐"]
+
 
 def get_scores():
     scores = {}
 
     for subject in SUBJECTS:
-        score = float(input(f"{subject} score: "))
+        score = float(input(f"{subject} 점수: "))
         scores[subject] = score
 
     return scores
@@ -14,9 +15,18 @@ def calculate_average(scores):
     return sum(scores.values()) / len(scores)
 
 
+def print_results(scores, average):
+    print("점수 결과")
+
+    for subject, score in scores.items():
+        print(f"{subject}: {score}")
+
+    print("평균:", average)
+
+
 if __name__ == "__main__":
     scores = get_scores()
 
     average = calculate_average(scores)
 
-    print("Average:", average)
+    print_results(scores, average)
