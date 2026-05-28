d4ff548521ae3f44b84a033a0b643f95238194e1
mahtab5502 <mahtabdehkordi20408m@gmail.com>
2026-05-28 18:01:50 +0330

    Add average calculation

--- grades.py
+++ grades.py
@@ -1,21 +1,22 @@
-SUBJECTS = ["국어", "영어", "수학", "과탐"]
+SUBJECTS = ["Math", "English", "Science"]
 
 def get_scores():
     scores = {}
 
     for subject in SUBJECTS:
-        score = float(input(f"{subject} 점수: "))
+        score = float(input(f"{subject} score: "))
         scores[subject] = score
 
     return scores
 
+
 def calculate_average(scores):
     return sum(scores.values()) / len(scores)
+
 
 if __name__ == "__main__":
     scores = get_scores()
 
     average = calculate_average(scores)
 
-    print(scores)
-    print("평균:", average)
+    print("Average:", average)
